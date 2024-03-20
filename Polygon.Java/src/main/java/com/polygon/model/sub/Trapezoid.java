ce629395f90812799ecae890b0235ef1bb4bed21
KierraB <kierrabroadnax20@gmail.com>
2024-03-20 18:36:06 -0500

    Polygons and Quads

---
+++ Polygon.Java/src/main/java/com/polygon/model/sub/Trapezoid.java
@@ -0,0 +1,43 @@
+package com.polygon.model.sub;
+
+import com.polygon.model.base.Quadrilateral;
+
+public class Trapezoid extends Quadrilateral {
+
+    private final int angle;
+
+    public Trapezoid(double sideA, double sideC, double sideB, double sideD, int angle){
+        super(sideA, sideC, sideB, sideD);
+        this.angle = angle;
+    }
+
+    @Override
+    public double getHeight(){
+        return getSideC() * Math.sin(Math.toRadians(angle));
+    }
+
+    @Override
+    public double getArea(){
+        return .5 * (getSideA() + getSideB() * getHeight());
+    }
+
+    @Override
+    public double getPerimeter(){
+        return getSideA()+ getSideB() + getSideC() + getSideD();
+    }
+
+    @Override
+    public String toString(){
+        return "Trapezoid";
+    }
+
+
+
+
+
+
+
+
+
+
+}
